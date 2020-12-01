eb680257c5fc50ed7917d976ab6b422e0d62596f
Manuel Schmid (schmim62) <schmim62@students.zhaw.ch>
2020-12-01 08:09:37 +0100

    Added P11

---
+++ P_11_RTOS/code/P_Button/RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,18 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Button' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+        #define RTE_Compiler_IO_STDOUT_User     /* Compiler I/O: STDOUT User */
+
+#endif /* RTE_COMPONENTS_H */
